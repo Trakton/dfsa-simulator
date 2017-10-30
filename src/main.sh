ef2b766387a15f5020a37a2e2853605cef0dc243
Trakton <higor.cmbotelho@gmail.com>
2017-10-30 18:39:50 -0300

    Changing from line to linespoints in graph

--- src/main.sh
+++ src/main.sh
@@ -10,9 +10,11 @@ gnuplot <<- EOF
   set xlabel "Tags"
   set ylabel "Slots"
   set title "Total Number of Slots used for each Estimator"
+  set style line 1 pt 1
+  set style line 2 pt 2
   set term png
   set output "graphs/slots.png"
-  plot "output/slots.csv" u 1:2 title "LowerBound" w l, "output/slots.csv" u 1:3 title "Eom Lee" w l
+  plot "output/slots.csv" u 1:2 pt 1 title "LowerBound" w linespoints, "output/slots.csv" u 1:3 title "Eom Lee" w linespoints
 EOF
 
 gnuplot <<- EOF
@@ -23,7 +25,7 @@ gnuplot <<- EOF
   set title "Total Number of Empty Slots left for each Estimator"
   set term png
   set output "graphs/empties.png"
-  plot "output/empties.csv" u 1:2 title "LowerBound" w l, "output/empties.csv" u 1:3 title "Eom Lee" w l
+  plot "output/empties.csv" u 1:2 title "LowerBound" w linespoints, "output/empties.csv" u 1:3 title "Eom Lee" w linespoints
 EOF
 
 gnuplot <<- EOF
@@ -33,7 +35,7 @@ gnuplot <<- EOF
   set title "Total Number of Collisions ocurred for each Estimator"
   set term png
   set output "graphs/collisions.png"
-  plot "output/collisions.csv" u 1:2 title "LowerBound" w l, "output/collisions.csv" u 1:3 title "Eom Lee" w l
+  plot "output/collisions.csv" u 1:2 title "LowerBound" w linespoints, "output/collisions.csv" u 1:3 title "Eom Lee" w linespoints
 EOF
 
 gnuplot <<- EOF
@@ -43,5 +45,5 @@ gnuplot <<- EOF
   set title "Total Simulation Runtime in Microseconds"
   set term png
   set output "graphs/time.png"
-  plot "output/time.csv" u 1:2 title "LowerBound" w l, "output/time.csv" u 1:3 title "Eom Lee" w l
+  plot "output/time.csv" u 1:2 title "LowerBound" w linespoints, "output/time.csv" u 1:3 title "Eom Lee" w linespoints
 EOF
